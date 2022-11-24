1cf4f27aa60eea33a7f2cbd2dfcb61eeb44b8956
luiseduardopcpereira <luiseduardopcpereira@gmail.com>
2022-11-23 22:29:31 -0300

    synchronizing database

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,3 +1,20 @@
 const mongosse = require("mongoose")
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASS;
 
-// connection
+
+
+const conn = async () => {
+    try {
+        const dbConn = await mongosse.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.fwkxnnt.mongodb.net/?retryWrites=true&w=majority`);
+        console.log("Conenctou ao BD!")
+
+        return dbConn
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+conn();
+
+module.exports = conn;
